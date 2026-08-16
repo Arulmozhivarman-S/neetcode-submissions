8d516afc342b8afa47e61f859eaf1dcd26364686
Arulmozhivarman-S <124854685+Arulmozhivarman-S@users.noreply.github.com>
2026-08-16 17:11:12 +0530

    Add: merge-triplets-to-form-target - submission-1

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& tri, vector<int>& t) {
+        
+        vector<int> ans(3, 0);
+        sort(tri.begin(), tri.end());
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<tri.size(); j++){
+                vector<int> &cur = tri[j];
+                if(cur[i]==t[i]){
+                    ans[0] = max(ans[0], cur[0]);
+                    ans[1] = max(ans[1], cur[1]);
+                    ans[2] = max(ans[2], cur[2]);
+                    break;
+                }
+            }
+        }
+       
+        return ans==t;
+
+    }
+};
