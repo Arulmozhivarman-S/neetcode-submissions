6976dd3fabcb9012bc7785a94156d05eece1479f
Arulmozhivarman-S <124854685+Arulmozhivarman-S@users.noreply.github.com>
2026-08-20 19:33:49 +0530

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        
+        int n = mat.size(), m = mat[0].size();
+        
+        reverse(mat.begin(), mat.end());
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<m; j++){
+                swap(mat[i][j], mat[j][i]);
+            }
+        }
+        
+    }
+};
